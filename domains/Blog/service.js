7b1f81b36af2d23c3a29af4ba9752cdbaf2bb77f
Flavio <flavioribeirojr@outlook.com>
2019-03-10 11:43:24 -0300

    Add post category filter

--- domains/Blog/service.js
+++ domains/Blog/service.js
@@ -1,3 +1,43 @@
 const posts = [
-  
+  {
+    title: 'The life of a fisherman',
+    description: 'Minima expedita sunt placeat dolore, aspernatur ullam! Libero veniam dolor vero. Harum, iste perferendis autem molestias obcaecati quis ipsum aut omnis repellat.',
+    image: '/static/images/fisherman.jpg',
+    category_id: 1
+  },
+  {
+    title: 'Meet our city wonders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
+    image: '/static/images/houses.jpg',
+    category_id: 1
+  },
+  {
+    title: 'Meet our city wonders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
+    image: '/static/images/houses.jpg',
+    category_id: 3
+  },
+  {
+    title: 'The life of a fisherman',
+    description: 'Minima expedita sunt placeat dolore, aspernatur ullam! Libero veniam dolor vero. Harum, iste perferendis autem molestias obcaecati quis ipsum aut omnis repellat.',
+    image: '/static/images/fisherman.jpg',
+    category_id: 4
+  },
+  {
+    title: 'Meet our city wonders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
+    image: '/static/images/houses.jpg',
+    category_id: 2
+  },
+  {
+    title: 'Meet our city wonders',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
+    image: '/static/images/houses.jpg',
+    category_id: 3
+  }
 ]
+
+export const getPosts = (category = null) =>
+  new Promise(resolve => setTimeout(() => {
+    resolve(category ? posts.filter(post => post.category_id === category) : posts)
+  }, 1000))
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,31 +2,21 @@ import React, { useState, useEffect } from 'react'
 import Dynamic from 'next/dynamic'
 import AppWrapper from 'pages/AppWrapper'
 import SpotlightArticlePreview from '@/SpotlightArticlePreview'
+import ArticlePreview from '@/ArticlePreview';
 import { getCategories } from 'domains/Blog/Categories/service'
+import { getPosts } from 'domains/Blog/service'
 import styles from './Blog.scss'
 import theme from 'pages/theme.scss'
 
 const Header = Dynamic(import('@/Header'))
 
-const posts = [
-  {
-    title: 'The life of a fisherman',
-    description: 'Minima expedita sunt placeat dolore, aspernatur ullam! Libero veniam dolor vero. Harum, iste perferendis autem molestias obcaecati quis ipsum aut omnis repellat.',
-    image: '/static/images/fisherman.jpg'
-  },
-  {
-    title: 'Meet our city wonders',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
-    image: '/static/images/houses.jpg'
-  },
-  {
-    title: 'Meet our city wonders',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus suscipit eum veritatis enim sint quam distinctio facere at',
-    image: '/static/images/houses.jpg'
-  }
-]
+const Blog = ({ categories, posts: initialPosts }) => {
+  const [ posts, setPosts ] = useState(initialPosts);
 
-const Blog = ({ categories }) => {
+  const filterPosts = (categoryId) =>
+    getPosts(categoryId)
+      .then(posts => setPosts(posts))
+
   return (
     <AppWrapper>
       <Header isHome={false} />
@@ -39,44 +29,80 @@ const Blog = ({ categories }) => {
             {
               categories
                 .map((category, idx) => (
-                  <li key={idx}>
+                  <li
+                    key={idx}
+                    onClick={() => filterPosts(category.id)}
+                  >
                     { category.name }
                   </li>
                 ))
             }
           </ul>
         </nav>
-        <div className={`${theme.row} ${styles.articles}`}>
-          <div className={`${theme.column} ${theme.is7}`}>
-            <SpotlightArticlePreview
-              {...posts[0]}
-              showBig
-            />
-          </div>
-          <div className={`${theme.column} ${theme.is4}`}>
-            {
-              posts
-                .filter((_, idx) => idx !== 0)
-                .map((post, idx) => (
-                  <div className={`${theme.column} ${styles.tinyArticle}`}>
-                    <SpotlightArticlePreview
-                      key={idx}
-                      {...post}
-                    />
+        {
+          posts.length > 0 && (
+            <>
+              <div className={`${theme.row} ${styles.articles}`}>
+                <div className={`${theme.column} ${ posts.length === 1 ? theme.is12 : theme.is7 }`}>
+                  <SpotlightArticlePreview
+                    {...posts[0]}
+                    showBig
+                  />
+                </div>
+                <div className={`${theme.column} ${theme.is4}`}>
+                  {
+                    posts
+                      .slice(1, 3)
+                      .map((post, idx) => (
+                        <div className={`${theme.column} ${styles.tinyArticle}`}>
+                          <SpotlightArticlePreview
+                            key={idx}
+                            {...post}
+                          />
+                        </div>
+                      ))
+                  }
+                </div>
+              </div>
+              {
+                posts.length > 3 && (
+                  <div className={styles.oldArticles}>
+                    <hr />
+                    <div className={theme.row}>
+                      {
+                        posts
+                          .slice(3)
+                          .map((post, idx) => (
+                            <div
+                              key={idx}
+                              className={`${theme.column} ${theme.is4}`}
+                            >
+                              <ArticlePreview
+                                {...post}
+                              />
+                            </div>
+                          ))
+                      }
+                    </div>
                   </div>
-                ))
-            }
-          </div>
-        </div>
+                )
+              }
+            </>
+          )
+        }
       </section>
     </AppWrapper>
   )
 }
 
 Blog.getInitialProps = () => new Promise(resolve => (
-  getCategories().then(categories => resolve({
-    categories
-  }))
+  getCategories().then(categories => 
+    getPosts()
+      .then(posts => resolve({
+        posts,
+        categories
+      }))
+  )
 ))
 
 export default Blog
